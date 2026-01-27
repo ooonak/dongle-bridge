96133bfe6215ee6619c6c73aba91650f694de851
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2026-01-28 00:51:44 +0100

    feat(test): We need to be able to test.

--- app/src/main.c
+++ app/src/main.c
@@ -1,11 +1,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include "sum.h"
 
 LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
     LOG_INF("Hello, Zephyr");
+
+    printf("2 + 3 = %d\n", add(2, 3));
+
     return 0;
 }
 
